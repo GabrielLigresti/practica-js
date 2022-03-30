b411cb1ed019bd1ff111e16a3b8208a889d67e5f
GabrielLigresti <gabriel.ligresticruz@gmail.com>
2022-03-30 20:40:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-gi4ntw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-gi4ntw)
---
+++ ejercicios/ejercicio1.js
@@ -0,0 +1,15 @@
+/**
+ * Hacer una funcion que dado un array de participantes del curso me devuelva el alumno con mayor edad.
+ * 
+ * [
+ *  {
+ *    firstName,
+ *    lastName,
+ *    age
+ *  },
+ *  ...,
+ *  n
+ * ]
+ */
+
+
